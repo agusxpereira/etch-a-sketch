728f7ac883a8e1c02c6a835d2d3576168cdbb054
Agustin Pereira <aguspereira@protonmail.com>
2023-12-21 17:40:38 -0300

    Version para subir a odin

--- main.js
+++ main.js
@@ -1,71 +1,94 @@
+let userPick = -1;
+let color = document.querySelector('#color-picker').value;
+let rep = (userPick < 100 && userPick > 0) ? userPick: 32;
+let inter = -1; 
+
 let display = document.querySelector('.display-grid'); 
 let buttonCreate = document.querySelector('#grid-create');
 let buttonClear = document.querySelector('#clear');
 let buttonUserPick = document.querySelector('#user-pick');
+let buttonRainbow = document.querySelector('#rainbow-mode');
+let buttonStop = document.querySelector('#stop');
 
-
-let userPick = -1;
-buttonUserPick.addEventListener("click",()=>{
-    display.innerHTML = "";
-    userPick = prompt("give me a number for youre etch!", 0);
-});
-
-
-
-
-
-
-buttonCreate.addEventListener('click', ()=>{
-    let rep = (userPick < 100 && userPick > 0) ? userPick: 32;
-    console.log("click");
+function createGrid(rep=32){
+    cant = (100 / rep);
     if(display.innerHTML == ""){
         for(let i = 0; i < rep; i++){    
             for (let j = 0; j < rep; j++) {       
                let divSquare = document.createElement('div'); 
                 divSquare.classList.add("square");
-                cant = (100 / rep); 
                 divSquare.style.height =  cant  + "%";
                 divSquare.style.width = cant + "%";                
                 display.appendChild(divSquare);                                 
-            }    
-        }
+            };   
+        };
     }
-        let divSquares = document.querySelectorAll('.square'); 
-        let isDrawing = false;
-        divSquares.forEach(div =>  {
-                function draw(e) {
-                    let colorPick = document.querySelector('#color-picker').value;
-                    
-                    e.target.style.backgroundColor = colorPick;
-                }
+}//create grid 
+function drawing(e, color="black") {     
+    e.target.style.backgroundColor = color;
+}
 
-                div.addEventListener("mousedown", (e)=>{
-                    isDrawing = true;
-                    
-                });
-                div.addEventListener("mouseover", (e)=>{
-                    
-                    if(isDrawing == true){
-                       draw(e);
-                    }
-                });
-                div.addEventListener("mouseup", (e)=>{
-                    isDrawing = false;
-                    
-                });
-
- 
+function draw(color="black"){
+    let divSquares = document.querySelectorAll('.square'); 
+    let isDrawing = false;
+    divSquares.forEach(div =>  {
+        div.addEventListener("mousedown", (e)=>{
+            isDrawing = true;                
         });
+        div.addEventListener("mouseover", (e)=>{                 
+            if(isDrawing == true){
+                drawing(e, color);
+            }
+        });
+        div.addEventListener("mouseup", (e)=>{
+            isDrawing = false;                   
+        });
+    });
+}//draw
+    function randomColor(){
+        let simbolos, color;
+        simbolos = "0123456789ABCDEF";
+        color = "#";
 
-}); 
-
+        for(var i = 0; i < 6; i++){
+            color = color + simbolos[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
 
 //clear 
 buttonClear.addEventListener("click", ()=>{
     display.innerHTML = "";
+    createGrid();
+    draw(color);
 }); 
 
+buttonUserPick.addEventListener("click",()=>{
+    display.innerHTML = "";
+    userPick = prompt("Select a number for your board!", 0);
+    createGrid(userPick);
+    draw();
+}); 
 
+buttonRainbow.addEventListener("click", ()=>{ 
+    
+    if(inter){
+        let color = ""; 
+        inter = setInterval(()=>{
+            color = randomColor();
+            console.log(color);
+            console.log(inter);
+            draw(color);
+        }, 100);
+    }
+    
+    buttonStop.addEventListener("click", ()=>{
+        clearInterval(inter);
+        draw(color);
+    });
+    
+});
 
-
+createGrid(rep);
+draw(color);
 /*https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event */
